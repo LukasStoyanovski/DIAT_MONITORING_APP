f677cee324875ccf8851830f94df81bf2816e6da
Lukas Stojanovski <lstoyanovski@york.citycollege.eu>
2024-11-19 03:34:42 +0200

    Added navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet,  TextInput } from "react-native";
+import { Link, Stack } from 'expo-router';
+import { BorderlessButton } from "react-native-gesture-handler";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
     >
-      <Text>Diat Monitoring App </Text>
+    <Link href="/my_profile" style={styles.link}>Go to my Profile</Link>
+      <Text>Welcome to the Diat Monitoring App </Text>
+      <Text style={styles.title}>Log in </Text>
+      <TextInput style={styles.input}></TextInput>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+    fontSize: 20,
+
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    marginTop: 20,
+    width: '80%',
+    paddingLeft: 10,
+  },
+  title: {
+    fontSize: 50,
+  }
+});
---
+++ app/my_profile.tsx
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default function MyProfile() {
+    return(
+        <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Text>This is My Profile</Text>
+    </View>
+    )
+}
